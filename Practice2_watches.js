03137f882ef91a9b85fd9493ea162dac1be6a467
bhrnw <afleg@me.com>
2021-02-14 22:11:05 +0100

    big update

---
+++ Practice2_watches.js
@@ -0,0 +1,14 @@
+let totalCash = prompt("How much money you got?");
+let watchesCount = prompt("How many watches you wanna get?");
+let earringCount = prompt("How many earrings you wanna get?");
+let watchesSum = prompt("How much are the watches?")
+let earringSum = prompt("How much are the earrings?")
+let num1 = parseInt(totalCash);
+let num2 = parseInt(watchesCount);
+let num3 = parseInt(earringCount);
+let num4 = parseInt(watchesSum);
+let num5 = parseInt(earringSum);
+num1 >= num2*num4 + num3*num5
+
+let totalCash = parselIt( prompt("How much money you got?") );
+
---
+++ Practice3/js/script.js
@@ -0,0 +1,6 @@
+let totalCash = parseInt(prompt("How much money you got?"));
+let watchesCount = parseInt(prompt("How many watches you wanna get?"));
+let earringCount = parseInt(prompt("How many earrings you wanna get?"));
+let watchesSum = parseInt(prompt("How much are the watches?"));
+let earringSum = parseInt(prompt("How much are the earrings?"));
+totalCash >= watchesCount*watchesSum + earringCount*earringSum
